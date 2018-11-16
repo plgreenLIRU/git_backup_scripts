046d4911f54474cdabd0c07ae77ceb6a23c187a1
Peter Green <p.l.green@liverpool.ac.uk>
2018-11-16 09:18:44 +0000

    First commit

---
+++ git_backup.bat
@@ -0,0 +1,29 @@
+
+:: Change to the directory where you want to back up files.
+cd C:\Users\Pete\Documents\git_backups
+
+:: Make a dated directory for backing up files from git hub. 
+mkdir git_backup_%date:/=%
+
+:: Change into the dated directory.
+cd git_backup_%date:/=%
+
+:: Backup everything you want to using standard git commands
+git clone https://github.com/plgreenLIRU/GP-Regression-Development
+git clone https://github.com/plgreenLIRU/MCMC
+git clone https://github.com/plgreenLIRU/test
+git clone https://github.com/plgreenLIRU/French
+git clone https://github.com/plgreenLIRU/LIFE2
+git clone https://github.com/plgreenLIRU/AM_paper
+git clone https://github.com/sgrjack2/GUI
+git clone https://github.com/plgreenLIRU/GMM
+git clone https://github.com/plgreenLIRU/Gaussian-Process-regression-tutorial
+git clone https://github.com/plgreenLIRU/Gaussian-Process-SparseGP-Tutorial
+git clone https://github.com/plgreenLIRU/git_backup_scripts
+
+
+
+
+
+
+
